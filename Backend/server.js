0385c473b4adcd789c3e81a121156dd80b8ca4c4
Abhay <abhaypratapsingh7704866570@gmail.com>
2026-03-22 23:22:50 +0530

    Setup

---
+++ Backend/server.js
@@ -0,0 +1,13 @@
+import express from 'express'
+const app = express()
+import dotenv from 'dotenv'
+dotenv.config()
+const port = process.env.port || 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
